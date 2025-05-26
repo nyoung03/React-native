ade69e2bfd675d94f2b81c5ae6a00b2c04477607
nyoung03 <vnfdlvdk37@gmail.com>
2025-05-26 23:09:33 +0900

    컬러 변수 추가가

--- constants/index.ts
+++ constants/index.ts
@@ -2,6 +2,13 @@ const colors = {
   WHITE: "#fff",
   BLACK: "#000",
   ORANGE_600: "#FF6B57",
+
+  GRAY_100: "#f6f6f6",
+  GRAY_200: "#E2E8F0",
+  GRAY_300: "#D1D5DB",
+  GRAY_500: "#6B7280",
+  GRAY_600: "#4B5563",
+  GRAY_700: "#374151",
 };
 
 export { colors };

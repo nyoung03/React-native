78e58dae0c7f70408330db87f5245d20da670f51
nyoung03 <vnfdlvdk37@gmail.com>
2025-05-28 22:54:53 +0900

    FlatList

---
+++ components/FeedItem.tsx
@@ -0,0 +1,86 @@
+import { colors } from "@/constants";
+import { Post } from "@/types";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import Entypo from "@expo/vector-icons/Entypo";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import React from "react";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+
+interface FeedItemProps {
+  post: Post;
+}
+
+function FeedItem({ post }: FeedItemProps) {
+  const isLiked = false;
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.contentContainer}>
+        <Text style={styles.title}>{post.title}</Text>
+        <Text numberOfLines={3} style={styles.description}>
+          {post.description}
+        </Text>
+      </View>
+      <View style={styles.menuContainer}>
+        <Pressable style={styles.menu}>
+          <Entypo
+            name={isLiked ? "heart" : "heart-outlined"}
+            size={16}
+            color={isLiked ? colors.ORANGE_600 : colors.BLACK}
+          />
+          <Text style={isLiked ? styles.activeMenuText : styles.menuText}>
+            1
+          </Text>
+        </Pressable>
+        <Pressable style={styles.menu}>
+          <FontAwesome name="commenting-o" size={16} color={colors.BLACK} />
+          <Text>1</Text>
+        </Pressable>
+        <Pressable style={styles.menu}>
+          <AntDesign name="eyeo" size={16} color={colors.BLACK} />
+          <Text>1</Text>
+        </Pressable>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.WHITE,
+  },
+  contentContainer: { padding: 16 },
+  title: {
+    fontSize: 18,
+    color: colors.BLACK,
+    fontWeight: "600",
+    marginVertical: 8,
+  },
+  description: { fontSize: 16, color: colors.BLACK, marginBottom: 14 },
+  menuContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around",
+    borderTopColor: colors.GRAY_300,
+    borderTopWidth: StyleSheet.hairlineWidth,
+  },
+
+  menu: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 16,
+    width: "33%",
+    gap: 4,
+  },
+  menuText: {
+    fontSize: 14,
+    color: colors.GRAY_700,
+  },
+  activeMenuText: {
+    fontWeight: "500",
+    color: colors.ORANGE_600,
+  },
+});
+
+export default FeedItem;
---
+++ components/FeedList.tsx
@@ -0,0 +1,63 @@
+import { colors } from "@/constants";
+import { Post } from "@/types";
+import React from "react";
+import { FlatList, StyleSheet } from "react-native";
+import FeedItem from "./FeedItem";
+
+interface FeedListProps {
+  data: Post[];
+}
+
+const dummy = [
+  {
+    id: 1,
+    userId: 1,
+    title: "더미",
+    description:
+      "더미 내용더미 내용더미 내용더미 내용더미 내용더미 내용더미 내용더미 내용더미 내용더미 내용더미 내용더미 내용더미 내용더미 내용더미 내용더미 내용더미 내용더미 내용더미 내용더미 내용더미 내용",
+    createdAt: "",
+    author: { id: 1, nickname: "nickname", imageUri: "" },
+    imageUris: [],
+    likes: [],
+    hasVote: false,
+    voteCount: 2,
+    commentCount: 2,
+    viewCount: 3,
+  },
+  {
+    id: 1,
+    userId: 1,
+    title: "더미",
+    description:
+      "더미 내용더미 내용더미 내용더미 내용더미 내용더미 내용더미 내용더미 내용더미 내용더미 내용더미 내용더미 내용더미 내용더미 내용더미 내용더미 내용더미 내용더미 내용더미 내용더미 내용더미 내용",
+    createdAt: "",
+    author: { id: 1, nickname: "nickname", imageUri: "" },
+    imageUris: [],
+    likes: [],
+    hasVote: false,
+    voteCount: 2,
+    commentCount: 2,
+    viewCount: 3,
+  },
+];
+
+function FeedList() {
+  return (
+    <FlatList
+      data={dummy}
+      renderItem={({ item }) => <FeedItem post={item} />}
+      keyExtractor={(item) => String(item.id)}
+      contentContainerStyle={styles.contentContainer}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  contentContainer: {
+    paddingVertical: 12,
+    backgroundColor: colors.GRAY_200,
+    gap: 12,
+  },
+});
+
+export default FeedList;
